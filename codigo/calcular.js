cd1a37672d8de1ef903b5dedbe2b1c48a3bde3ba
alejandro <alejandrosermiranda@gmail.com>
2022-09-14 20:52:24 -0500

    primera actualizacion

---
+++ codigo/calcular.js
@@ -0,0 +1,18 @@
+function calcular() {
+
+    let viajes = document.getElementById("viajes").value;
+    let pasajeros = document.getElementById("pasajeros").value;
+    let total = 0;
+    total = viajes * pasajeros;
+    if (viajes == "medellin") {
+
+        // total=10000*pasajeros
+        // resultado1.innerHTML = "el resultado es :" + total;
+        // document.write(resultado1)
+        let resultado1 = document.getElementById("respuesta");
+        resultado1.innerHTML = "el resultado es :" + total;
+
+    }
+
+
+}
