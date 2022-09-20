b737b9a9c203030c5d38873808350725d9a8559e
alejandro <alejandrosermiranda@gmail.com>
2022-09-19 22:10:45 -0500

    Segunda actualizacion

--- codigo/calcular.js
+++ codigo/calcular.js
@@ -1,23 +1,53 @@
-function calcular() {
-
+function   calcular() {
+   let salida;
+    let mostrar=document.getElementById("respuesta");
     let viajes = document.getElementById("viajes").value;
-    let pasajeros = document.getElementById("pasajeros").value;
+    let pasajeros =Number( document.getElementById("pasajeros").value)
+    let maletas=Number(document.getElementById("peso").value)
+    let tipoSilla = document.getElementById("silla").value;
     let total = 0;
+    let pesoAdicional=0;
+    let totalSilla = 0;
     total = viajes * pasajeros;
-    if (viajes == "medellin") {
+    if (viajes == 1) {
 
-        // total=10000*pasajeros
-        // resultado1.innerHTML = "el resultado es :" + total;
-        // document.write(resultado1)
-        let resultado1 = document.getElementById("respuesta");
-        resultado1.innerHTML = "el resultado es :" + total;
+       total=100000*pasajeros
+       mostrar.innerHTML = "el total del pasaje es :" + total;
+      
+       
+        
+    }
+    salida=total;
+    if (viajes==2){
 
+        total=200000*pasajeros
+        mostrar.innerHTML="el total del pasaje es: "+total;
     }
 
-    if (viajes=="pereira"){
+    if (viajes==3){
 
-        total=2000*4
+        total=250000*pasajeros
+        mostrar.innerHTML="el total del pasaje es: "+total;
     }
 
+    if (viajes==4){
 
+        total=180000*pasajeros
+        mostrar.innerHTML="el total del pasaje es: "+total;
+    }
+
+    if(maletas>50){
+pesoAdicional=(maletas-50)*15000
+
+    }
+    // Tipo de silla
+  if (tipoSilla == "Normal") {
+    totalSilla = 0 * pasajeros;
+  }
+  if (tipoSilla == "Ejecutiva") {
+    totalSilla = 20000 * pasajeros;
+  }
+  if (tipoSilla == "VIP") {
+    totalSilla = 40000 * pasajeros;
+    }
 }
